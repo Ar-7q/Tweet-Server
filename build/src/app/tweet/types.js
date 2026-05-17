5f9eb8c264b288495357adb51c79dea873ec2679
Ar-7q <arpitrai927@gmail.com>
2026-05-17 21:19:05 +0530

    added createdate in types.ts at tweet

--- build/src/app/tweet/types.js
+++ build/src/app/tweet/types.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.types = void 0;
-exports.types = "#graphql\n\n    type ImageResponse {\n        imageURL: String\n        imagePublicId: String\n    }\n\n    input CreateTweetData {\n        content:String!\n        imageURL:String\n        imagePublicId:String\n    }\n\n    type Tweet {\n        id:ID!\n        content:String!\n        imageURL:String\n        imagePublicId:String\n        author:User\n    }\n";
+exports.types = "#graphql\n\n    type ImageResponse {\n        imageURL: String\n        imagePublicId: String\n    }\n\n    input CreateTweetData {\n        content:String!\n        imageURL:String\n        imagePublicId:String\n    }\n\n    type Tweet {\n        id:ID!\n        content:String!\n        imageURL:String\n        imagePublicId:String\n        createdAt:String\n        author:User\n    }\n";
--- src/app/tweet/types.ts
+++ src/app/tweet/types.ts
@@ -16,6 +16,7 @@ export const types = `#graphql
         content:String!
         imageURL:String
         imagePublicId:String
+        createdAt:String
         author:User
     }
 `;
