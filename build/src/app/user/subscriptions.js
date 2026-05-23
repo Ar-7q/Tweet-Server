9c24f1beea99f13a7a9038e23405fe10e7939cb3
Ar-7q <arpitrai927@gmail.com>
2026-05-24 01:08:59 +0530

    remove build folder from github

--- build/src/app/user/subscriptions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userSubscriptions = void 0;
-const pubsub_1 = require("../../graphql/pubsub");
-exports.userSubscriptions = {
-    userFollowed: {
-        subscribe: () => pubsub_1.pubsub.asyncIterableIterator(["USER_FOLLOWED"]),
-    },
-};
