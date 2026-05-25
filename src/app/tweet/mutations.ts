2d835783703f59cfad7f14824659b2e4e853364a
Ar-7q <arpitrai927@gmail.com>
2026-05-26 01:17:00 +0530

    comment out uploadImage mutation in Tweet resolvers

--- src/app/tweet/mutations.ts
+++ src/app/tweet/mutations.ts
@@ -4,7 +4,7 @@ export const mutations = `#graphql
 
     createTweet(payload: CreateTweetData!): Tweet
 
-    uploadImage(image: String!): ImageResponse
+    # uploadImage(image: String!): ImageResponse
     deleteTweet(tweetId: String!): Boolean
     toggleLike(tweetId: String!): Boolean
     createComment(tweetId:String!,content:String!):Comment
